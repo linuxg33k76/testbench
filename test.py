b84679ed3baead2f0d6bc2b91786f5b50c42fdcd
Ben Calvert <linuxg33k76@gmail.com>
2023-02-02 21:10:59 -0700

    added terminal interpreter

---
+++ test.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+from geopy import distance
+
+user_feet = 32
+
+center_point = [{'lat': -7.7940023, 'lng': 110.300000}]
+test_point = [{'lat': -7.7940023, 'lng': 110.300100}]
+radius = user_feet # in feet
+
+center_point_tuple = tuple(center_point[0].values())
+test_point_tuple = tuple(test_point[0].values())
+
+dis = distance.great_circle(center_point_tuple, test_point_tuple).feet #WGS 84 Earth Radius
+print(f'Distance: {dis:0.2f} ft')
+
+if dis <= radius:
+    print(f'{test_point_tuple} point is inside the {user_feet} ft radius from {center_point_tuple} coordinate')
+else:
+    print(f'{test_point_tuple} point is outside the {user_feet} ft radius from {center_point_tuple} coordinate')
